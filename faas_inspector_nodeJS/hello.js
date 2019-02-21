d0d5a4a97409124d21c48ec401cdbd6eb7602283
shuwen123 <shuwenroy@yahoo.com>
2019-02-20 18:56:04 -0800

    template framwork

--- faas_inspector_nodeJS/hello.js
+++ faas_inspector_nodeJS/hello.js
@@ -1,41 +1,9 @@
 exports.myHandler = function(event, context, callback){
     let name = event.name;
-    let vmbt = getUpTime();
-    let cpuType = getVmCpuStat();
-    let res = {
-        'name' : name,
-        'message' : "Hello " + name + ", welcome to node JS!",
-        'cpuType' : cpuType,
-        'vmuptime' : vmbt
-    }
+    const reg = require('./Register');
+    const myReg = new reg();
+    let res = myReg.profileVM();
+    res['name'] = event.name;
+    res['message'] = 'Hello ' + event.name + ' from Node JS Lambda!';
     context.succeed(res);
-}
-
-function getUpTime(){
-    // uses child process like subprocess in python.
-    // with spawnSync is Sync, without Sync is Async.
-    const { spawnSync } = require('child_process');
-    const { StringDecoder } = require('string_decoder');
-    const decoder = new StringDecoder('utf8');
-    // decoded from byte object into string already.
-    const child = spawnSync('grep', ['btime', '/proc/stat'], { encoding : 'utf8' });
-    let res = child.stdout;
-    res = res.replace('btime ', '');
-    res = res.trim();
-    res = parseInt(res);
-    return res;
-}
-
-function getVmCpuStat(){
-    // uses child process like subprocess in python.
-    // with execSync is Sync, without exec is Async.
-    const { execSync } = require('child_process');
-    const { StringDecoder } = require('string_decoder');
-    const decoder = new StringDecoder('utf8');
-    // decoded from byte object into string already.
-    const child = execSync('grep \'model name\' /proc/cpuinfo | head -1', { encoding : 'utf8' });
-    let res = child.replace('\n','');
-    res = res.replace('\t','');
-    res = res.replace('model name: ', '');
-    return res;
 }
